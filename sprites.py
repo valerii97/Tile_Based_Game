815226a09abcdee60e06e93f77335876fe468faa
valyanok <valyanok2014@gmail.com>
2020-10-27 17:19:10 +0100

    vectorized implementation of motion

--- sprites.py
+++ sprites.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from settings import *
 
+vec = pg.math.Vector2
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
@@ -10,53 +11,50 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.vx, self.vy = 0, 0  # this vars will be used to control player's speed
-        self.x = x * TILESIZE  # cause changed in update, need to make initial position of player
-        self.y = y * TILESIZE  # cause changed in update, need to make initial position of player
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y) * TILESIZE
 
     def get_keys(self):  # creating this method for more convenient change of controls in future
-        self.vx, self.vy = 0, 0  # for initial settings
+        self.vel = vec(0, 0)  # for initial settings
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
-            self.vx = -PLAYER_SPEED
+            self.vel.x = -PLAYER_SPEED
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
-            self.vx = PLAYER_SPEED
+            self.vel.x = PLAYER_SPEED
         if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.vy = PLAYER_SPEED
+            self.vel.y = PLAYER_SPEED
         if keys[pg.K_UP] or keys[pg.K_w]:
-            self.vy = -PLAYER_SPEED
-        if self.vx != 0 and self.vy != 0:  # to make diagonal movement at the same speed as in other dirs
-            self.vx *= 0.7071
-            self.vy *= 0.7071
+            self.vel.y = -PLAYER_SPEED
+        if self.vel.x != 0 and self.vel.y != 0:  # to make diagonal movement at the same speed as in other dirs
+            self.vel *= 0.7071
 
     def collide_with_walls(self, dir):
         if dir == 'x':
             hits = pg.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vx > 0:  # sprite moving to right
-                    self.x = hits[0].rect.left - self.rect.width  # this will stop us, while we moving to the right
-                if self.vx < 0:
-                    self.x = hits[0].rect.right
-                self.vx = 0  # if hits in x dir, than we must stop in this ax
-                self.rect.x = self.x
+                if self.vel.x > 0:  # sprite moving to right
+                    self.pos.x = hits[0].rect.left - self.rect.width  # this will stop us, while we moving to the right
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0  # if hits in x dir, than we must stop in this ax
+                self.rect.x = self.pos.x
         if dir == 'y':
             hits = pg.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vy > 0:
-                    self.y = hits[0].rect.top - self.rect.height
-                if self.vy < 0:
-                    self.y = hits[0].rect.bottom
-                self.vy = 0
-                self.rect.y = self.y
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.height
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0
+                self.rect.y = self.pos.y
 
     def update(self):
         self.get_keys()  # to check it at each frame
-        self.x += self.vx * self.game.dt  # frame independent movement
-        self.y += self.vy * self.game.dt
+        self.pos += self.vel * self.game.dt
         # Making 2 collision checks: one for each axes, to make possible sliding movement via walls' edges
-        self.rect.x = self.x
+        self.rect.x = self.pos.x
         self.collide_with_walls('x')
-        self.rect.y = self.y
+        self.rect.y = self.pos.y
         self.collide_with_walls('y')
 
 
